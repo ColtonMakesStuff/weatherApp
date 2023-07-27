a5460f65691806229a096b197f690e88108f1d32
Colton Firestone <colton.firestone@gmail.com>
2023-07-26 23:15:52 -0700

    bug fix and add readme

---
+++ assets/README.md
@@ -0,0 +1,28 @@
+# Weather App
+### This web application allows you to get the current weather and a five day forecast for any city in the world.
+
+### To use the app, simply enter the name of the city in the search bar and press enter or click on the search button. If you want to use a previous search, you can click on the name of the previous search.
+
+### The app will throw an error if you enter a name that is not a city name or if you try to search without inputting anything.
+
+## Features
+### Current weather
+### Five day forecast
+### Previous searches
+## How to use
+### Enter the name of the city in the search bar.
+### Press enter or click on the search button.
+### The current weather and five day forecast will be displayed.
+### To use a previous search, click on the name of the previous search.
+## Errors
+### If you enter a name that is not a city name, the app will throw an error.
+### If you try to search without inputting anything, the app will throw an error.
+## Requirements
+### A web browser
+### An internet connection
+
+## page link 
+### https://coltonmakesstuff.github.io/weatherApp/ Links to an external site.
+
+## screenshot
+## [ScreenShot](<assets/images/screenshot-of-website.png>)
--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,5 @@
 
+// lots of query selector's in arrears that pertain to certain sections of the HTML
 const city = document.querySelector(".city");
 const search = document.querySelector(".search");
 const temp = document.querySelector(".temp");
@@ -79,16 +80,17 @@ const storedUserData = localStorage.getItem('userData')
 let userDataArr = JSON.parse(storedUserData);
 
 
-
+// populates the history section on page load
 window.onload = function() {
     addToHistory()
-  };
+};
 
+// populates history section
   function addToHistory() {
     if (userDataArr === null) {
         userDataArr = [];
       }
-    // Get the stored userData from localStorage
+   
         const previousBox = document.querySelector('.idk');
         previousBox.innerHTML= ""
         if (userDataArr.length > 20) {
@@ -101,65 +103,57 @@ window.onload = function() {
         const prevSearchDiv = document.createElement('div');
         prevSearchDiv.classList.add('prev-search');
         prevSearchDiv.setAttribute('id', 'click-me');
-
         const pElement = document.createElement('p');
         pElement.textContent = userDataArr[index];
-        
-  
-
         prevSearchDiv.appendChild(pElement);
-  
-        // Append prevSearchDiv to a parent element in the DOM
-        
         previousBox.appendChild(prevSearchDiv);
-
-
-
         index ++
       }
     }
-const clickMe = document.querySelector('#click-me')
 
+// add event listener to history buttons
+const clickMe = document.querySelector('#click-me')
     clickMe.addEventListener('click', function() {
         console.log(event.target.textContent);
         let citySearch = event.target.textContent
-        fetchApi(citySearch)
+        fetchApiAgain(citySearch)
          
     });
   
-
+// this section handles time js
 var today = dayjs();
 currentTime.innerHTML = today.format('h:mm a');
 currentday.innerHTML = today.format('dddd') + " " + today.format('M/D/YYYY'); 
 
+//set key
 const apiKey = "3345d74a687b8041b547bb45348451f6";
 
+//establish lon lat
 var long = ''
 var lati = ''
+
 console.log(today.format('h:mm a'));
+
+//reload page
 function refresh(){
     location.reload();
 }
  
-  
+// this makes it so that the enter key can run the function  
 search.addEventListener("keypress", function(event) {
     if (event.key === "Enter") {
       event.preventDefault();
       logCity()
     }
-  });
-
+});
+// this starts off the function and passes the city input into the API, why is it called log city, I don't know
 function logCity(cityInput) {
     var cityInput = document.querySelector('#city-input').value;
     console.log(cityInput)
-    
     fetchApi(cityInput) 
     document.querySelector(".search").reset();
-
- 
-
 }
-
+// this is my error message
  function abducted() {
     city.innerHTML = "Please try again";    
     temp.innerHTML = "";
@@ -170,23 +164,18 @@ function logCity(cityInput) {
      humid.innerHTML = "oddly moist"
      weatherIcon.innerHTML = `<img class="weather-icon" src="./assets/images/icons/abducted.svg" alt="">`;
     fiveDaySection.classList.add('hidden');
-    
  }
 
 // Make the API request using the constructed URL
 function fetchApi(cityInput) {
   let cityName = cityInput;
-   
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`;
-
   fetch(apiUrl)
     .then((response) => {
       return response.json();
     })
-    
     .then((data) => {
         if (data.cod == '404' || data.cod == '400'){ 
-    //  alert("say that page was abjucted by aliens please try again") 
     abducted()
     return;
     }
@@ -206,13 +195,33 @@ function fetchApi(cityInput) {
       fetchFiveDayApi();
       addToHistory() 
     });
-   
 }
 
+// this function is used when a item in the history is clicked on so that it doesn't create a new history item
+function fetchApiAgain(cityInput) {
+    let cityName = cityInput; 
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`;
+    fetch(apiUrl)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+          if (data.cod == '404' || data.cod == '400'){ 
+      abducted()
+      return;
+      }
+      fiveDaySection.classList.remove('hidden');  
+        console.log(data);
+        handleData(data);
+        handleIcon(data);
+        fetchFiveDayApi();
+        addToHistory() 
+      });
+     
+  }
 
-
+// this sends all the pertinent data from the API request into the page
   function handleData(data){
-   
     city.innerHTML = data.name
     temp.innerHTML = Math.round((Math.round (data.main.temp))) + "°F";
     tempHigh.innerHTML = Math.round((Math.round (data.main.temp_max))) + "°F";
@@ -225,10 +234,9 @@ function fetchApi(cityInput) {
   }
 
 
-
+// this uses the latitude and longitude variables that I Had created in the handle data function, and puts them into a forecast API
   function fetchFiveDayApi() {
     const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lati}&lon=${long}&appid=${apiKey}&units=imperial`;
-  
     fetch(apiUrl)
       .then((response) => {
         return response.json();
@@ -238,59 +246,44 @@ function fetchApi(cityInput) {
       });
   }
 
-
+// this handles all of the updates to the site for the five smaller cards, the five day forecast
   function handleFiveDayApiData(data){
     console.log(data.list)
     let index = 0
     
     for (let i=0; i < data.list.length; i++){
-        
         if (data.list[i].dt_txt.substring(11, 13) == "00"){ 
-           
             console.log(data.list[i].dt_txt)
-           
            var timestamp = data.list[i].dt
            var date = dayjs.unix(timestamp);
            var dayOfWeek = date.format('dddd');
     console.log(dayOfWeek)
-
     dotwArr[index].innerHTML = dayOfWeek
-
                 fiveDayMaxArr[index].innerHTML = Math.round(data.list[i].main.temp_max)+ "°F";
                 console.log(index)
-                
                 fiveDayWindArr[index].innerHTML = "wind: " + Math.round(data.list[i].wind.speed/1.609344) + "mph";
                 fiveDayHumidArr[index].innerHTML = "humidity: " + (data.list[i].main.humidity) + "%";
-
-                
-                
                 var weather = data.list[i].weather[0].main
 
 console.log(weather)
 fiveDayIconArr[index].innerHTML = `<img src="./assets/images/icons/sunny.svg" alt="">`;
 
-                if (weather === 'Clouds'){
+            if (weather === 'Clouds'){
                     fiveDayIconArr[index].innerHTML = `<img src="./assets/images/icons/cloudy.svg" alt="">`;
-                    
-                } else if (weather === 'Rain'){
+            } else if (weather === 'Rain'){
                     fiveDayIconArr[index].innerHTML = `<img src="./assets/images/icons/rainy.svg" alt="">`;
-                            
-                } else if (weather === 'clear'){
-                    fiveDayIconArr[index].innerHTML = `<img src="./assets/images/icons/sunny.svg" alt="">`;
-                            
-                } else if (weather === 'snow'){
+            } else if (weather === 'clear'){
+                    fiveDayIconArr[index].innerHTML = `<img src="./assets/images/icons/sunny.svg" alt="">`;           
+            } else if (weather === 'snow'){
                   fiveDayIconArr[index].innerHTML = `<img src="./assets/images/icons/snow.svg" alt="">`;
-               }
-                
+            }
                 index ++;
-            
-           
         }
     }
   }
 
 
-
+// this sets the icon image for the main cards
   function handleIcon(data) {
     if(data.weather[0].main === 'Clouds'){
         weatherIcon.innerHTML = `<img class="weather-icon" src="./assets/images/icons/cloudy.svg" alt="">`;
@@ -309,25 +302,3 @@ fiveDayIconArr[index].innerHTML = `<img src="./assets/images/icons/sunny.svg" al
     }
 }
 
-
-//  function handleFiveDayIcon(index, data) {
-//      if(data.list[i].weather[0].main == 'Clouds'){
-//          fiveDayIconArr[index].innerHTML = `<img src="./assets/images/icons/cloudy.svg" alt="">`;
-
-//      } else if(data.list[i].weather[0].main == 'Rain'){
-//         fiveDayIconArr[index].innerHTML = `<img src="./assets/images/icons/rainy.svg" alt="">`;
-            
-//      } else if(data.list[i].weather[0].main == 'clear'){
-//         fiveDayIconArr[index].innerHTML = `<img src="./assets/images/icons/sunny.svg" alt="">`;
-        
-//      } else if(data.list[i].weather[0].main == 'snow'){
-//         fiveDayIconArr[index].innerHTML = `<img src="./assets/images/icons/snow.svg" alt="">`;
-    
-//  }
-   
-//  }
-
-
-function handlePreviousSearch() {
-
-}
